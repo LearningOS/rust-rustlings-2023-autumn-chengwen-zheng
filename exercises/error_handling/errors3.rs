e1b204ef27a952b4d4d5728012bb0b32f12914a6
chengwen-zheng <chengwenzheng1222@gmail.com>
2023-10-24 09:14:08 +0800

    feat: finish quiz3

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -15,7 +14,13 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input) {
+        Ok(cost) => cost,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -28,7 +33,10 @@ fn main() {
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
+    let qty = match item_quantity.parse::<i32>() {
+        Ok(qty) => qty,
+        Err(e) => return Err(e),
+    };
 
     Ok(qty * cost_per_item + processing_fee)
 }
